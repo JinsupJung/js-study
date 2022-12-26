57a2a96a44ebe7dae9730529d437aedc4f41cc93
JinsupJung <cool4uh@gmail.com>
2022-12-26 12:28:45 +0900

    1226 v1

--- colors.js
+++ colors.js
@@ -1,37 +1,46 @@
-var Links = {
-    setColor:function(color){
-      // var alist = document.querySelectorAll('a');
-      // var i = 0;
-      // while(i < alist.length){
-      //   alist[i].style.color = color;
-      //   i = i + 1;
-      // }
-      $('a').css('color', color);
-    }
-  }
-  var Body = {
-    setColor:function (color){
-      //document.querySelector('body').style.color = color;
-      $('body').css('color', color);
+var Body = {
+    setColor : function (color) {
+        document.querySelector('body').style.color = color;
     },
-    setBackgroundColor:function (color){
-      // document.querySelector('body').style.backgroundColor = color;
-      $('body').css('backgroundColor', color);
+    setBackgroudColor : function(color) {
+        document.querySelector('body').style.backgroundColor = color;
     }
-  }
-  function nightDayHandler(self){
+}
+var Links = {
+    setColor : function (color) {
+        var alist = document.querySelectorAll('a'); // all 임에 주의
+        var i = 0;
+        while (i < alist.length) {
+            // console.log(alist[i]); // 디버깅
+            // console.log(color);
+            alist[i].style.color = color;
+            i = i + 1;
+        }
+    }
+}
+function setColor(color) {
+    // console.log(color);
+    
+}
+function nightDayHandler(self) {
     var target = document.querySelector('body');
-    if(self.value === 'night'){
-      Body.setBackgroundColor('black');
-      Body.setColor('white');
-      self.value = 'day';
-  
-      Links.setColor('white');
-    } else {
-      Body.setBackgroundColor('white');
-      Body.setColor('black');
-      self.value = 'night';
-  
-      Links.setColor('blue');
+
+    if (self.value === 'night') {
+        Body.setBackgroudColor('black');
+        //target.style.backgroundColor='black';
+        Body.setColor('white');
+        // target.style.color='white';
+        self.value = 'day';
+
+        Links.setColor('yellow');
+        }
+
+    
+    else {
+        Body.setBackgroudColor('white');
+        Body.setColor('black');
+        self.value = 'night';
+
+        Links.setColor('blue');
     }
-  }
+}
